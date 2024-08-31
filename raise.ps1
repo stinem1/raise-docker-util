1326543c723c5256c0baa9ec9974294f3be49857
stinem1 <54391072+stinem1@users.noreply.github.com>
2024-08-31 10:23:42 +0200

    Give error messages and return exit code

--- raise.ps1
+++ raise.ps1
@@ -1,5 +1,15 @@
 $CMDS = $args[0..($args.Count - 2)]
+if ($CMDS -notcontains 'rsltc' -and $CMDS -notcontains 'sml') {
+    Write-Error "Error: No command specified. Please provide one of the following commands: rsltc, sml."
+    exit 1
+}
+
 $ARG = $args[-1]
+if ($ARG -eq $null -or -not (Test-Path -LiteralPath $ARG)) {
+    Write-Error "Error: The specified path is either missing or invalid. Please provide a valid file path."
+    exit 1
+}
+
 $REALDIR = (Get-Item -LiteralPath $ARG).FullName -replace '\\', '/'
 $DIR = Split-Path -Parent $REALDIR
 $NAME = Split-Path -Leaf $REALDIR
@@ -9,3 +19,4 @@ $NAME = Split-Path -Leaf $REALDIR
 
 # Run the Docker command with the correctly formatted path
 docker run --rm -v "${DIR}:/usr/src" ghcr.io/jakuj/raise-tools:main $CMDS "./$NAME"
+exit $LastExitCode
